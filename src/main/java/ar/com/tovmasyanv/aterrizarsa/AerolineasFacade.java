d82d7706145b99c6dab10968d55ddf4408faf270
vahanishe <vahanishe@users.noreply.github.com>
2015-11-09 17:39:45 -0300

    Se agrega la implementacion de avisarCambiosVuelo

--- src/main/java/ar/com/tovmasyanv/aterrizarsa/AerolineasFacade.java
+++ src/main/java/ar/com/tovmasyanv/aterrizarsa/AerolineasFacade.java
@@ -5,7 +5,19 @@ import java.util.Date;
 public class AerolineasFacade {
 
 	public void avisarCambiosVuelo(String codigoVuelo, Date fechaHoraSalidaNew, Date fechaHoraLlegadaNew) {
+		Empresa empresa = Empresa.getInstance();
+		List<Vuelo> vuelos = empresa.getVuelos();
 		
+		for(Vuelo vuelo : vuelos) {
+			if(codigoVuelo.equalsIgnoreCase(vuelo.getCodigoVuelo())) {
+				vuelo.setHoraSalida(fechaHoraSalidaNew);
+				vuelo.setHoraLlegada(fechaHoraLlegadaNew);
+				for(Pasaje pasaje : vuelo.getPasajesVendidos()) {
+					pasaje.getCliente().getMedioComunicacion().enviarMensaje(vuelo);
+				}
+				break;
+			}
+		}
 	}
 	
 	public void buscarPasajes(String destino, int cantidadPasajes, String tipoPasaje, Date fechaIda, Date fechaVuelta) {
