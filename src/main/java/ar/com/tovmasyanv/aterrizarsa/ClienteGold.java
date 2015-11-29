0b90a9c76e4744e40b88d53c637b7099fbc0a887
Vahanishe <vahanishe@hotmail.com>
2015-11-29 14:43:15 -0300

    reservarPasaje

--- src/main/java/ar/com/tovmasyanv/aterrizarsa/ClienteGold.java
+++ src/main/java/ar/com/tovmasyanv/aterrizarsa/ClienteGold.java
@@ -1,17 +1,30 @@
 package ar.com.tovmasyanv.aterrizarsa;
 
+import java.util.Date;
+
 import ar.com.tovmasyanv.dtos.DatosClienteDTO;
+import ar.com.tovmasyanv.exceptions.AterrizarException;
 
 public class ClienteGold extends Cliente {
+	
+	private final Integer RESERVA_ANTICIPACION = 8;
 
 	public ClienteGold(DatosClienteDTO datos) {
 		super(datos);
 	}
 
 	@Override
-	public void reservarPasaje(Vuelo vuelo, String tipo) {
-		// TODO Auto-generated method stub
-
+	public void reservarPasaje(Vuelo vuelo, String tipo) throws AterrizarException {
+		Date fechaActual = new Date();
+		long fechasDiferencia = vuelo.getHoraSalida().getTime() - fechaActual.getTime();
+		if(fechasDiferencia/(1000*60*60*24*30) > RESERVA_ANTICIPACION) {
+			int indexPasaje = vuelo.getPrimerMatch(tipo);
+			Pasaje pasaje = vuelo.getPasajesDisponibles().remove(indexPasaje);
+			pasaje.setCliente(this);
+			vuelo.getPasajesReservados().add(pasaje);
+		}
+		else
+			throw new AterrizarException("Usted puede reservar con el máximo de " + RESERVA_ANTICIPACION + " meses de antipacion");
 	}
 
 	@Override
